7f137b829877f9f01d112f7e1b7c92f21ce37617
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-07 19:52:16 -0400

    Added authorization with bcrypt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
 const app = express();
 app.use(express.json());
@@ -12,14 +13,37 @@ app.get("/users", (req, res) => {
   res.json(users);
 })
 
-app.post("/login", (req, res) => {
+app.post("/users/login", async (req, res) => {
+  console
+  const user = users.find(user => user.name = req.body.name);
+  if (!user) {
+    res.status(400).send("User does not exist!");
+  }
+  try {
+    if (await bcrypt.compare(req.body.pass, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Denied");
+    }
+  } catch {
+    res.status(500).send("Error");
+  }
 })
 
-app.post("/register", (req, res) => {
-  console.log(res.body);
-  const user = {name: req.body.name, password: req.body.pass};
-  users.push(user)
-  res.status(201).send();
+app.post("/users/register", async (req, res) => {
+  try {
+    const hashPass = await bcrypt.hash(req.body.pass, 10)
+    const exists = users.find(user => user.name = req.body.name);
+    const user = exists ?  null : {name: req.body.name, password: hashPass};
+    if (user) {
+      users.push(user)
+      res.status(201).send("Success");
+    } else {
+      res.status(409).send("User already exists")
+    }
+  } catch {
+    res.status(500).send("Error");
+  }
 })
 
 
