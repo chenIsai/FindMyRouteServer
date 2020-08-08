6769ea0e8aee74da10bab42d825caa712a4541d4
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-08 13:53:58 -0400

    Added working authServer

--- authServer.js
+++ authServer.js
@@ -1,40 +1,41 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt")
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = 4000;
 
 const users = [];
 
-app.get("/users", (req, res) => {
-  res.json(users);
-})
-
 app.post("/users/login", async (req, res) => {
-  console
-  const user = users.find(user => user.name = req.body.name);
-  if (!user) {
+  const exists = users.find(user => user.name = req.body.username);
+  if (!exists) {
     res.status(400).send("User does not exist!");
   }
   try {
-    if (await bcrypt.compare(req.body.pass, user.password)) {
-      res.send("Success");
+    if (await bcrypt.compare(req.body.password, exists.password)) {
+      console.log(exists.name);
+      const accessToken = jwt.sign({username: exists.name}, process.env.ACCESS_TOKEN_SECRET);
+      res.json({accessToken});
     } else {
       res.send("Denied");
     }
-  } catch {
-    res.status(500).send("Error");
+  } catch (e) {
+    res.status(500).send(e)
   }
+
+  const username = req.body.username;
+  const user = {username};
 })
 
 app.post("/users/register", async (req, res) => {
   try {
-    const hashPass = await bcrypt.hash(req.body.pass, 10)
-    const exists = users.find(user => user.name = req.body.name);
-    const user = exists ?  null : {name: req.body.name, password: hashPass};
+    const hashPass = await bcrypt.hash(req.body.password, 10)
+    const exists = users.find(user => user.username = req.body.username);
+    const user = exists ?  null : {name: req.body.username, password: hashPass};
     if (user) {
       users.push(user)
       res.status(201).send("Success");
