2b8ce07d3cd754e10acbfbc3d3e071ba6fb0a0d0
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-09 19:10:44 -0400

    Added routes for refreshing tokens and logging out

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1800s"})
+}
 
 module.exports.authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
@@ -20,6 +23,30 @@ module.exports.authenticateToken = (req, res, next) => {
     next();
   })
 }
+
+module.exports.refreshToken = (req, res) => {
+  const token = req.body.token;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  connection.query(`SELECT * FROM RefreshTokens WHERE value="${token}"`, (err, result) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    if (!result.length) {
+    res.sendStatus(403);
+    } else {
+      jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          res.sendStatus(403);
+        }
+        const accessToken = generateAccessToken({id: user.id});
+        res.status(200).json({accessToken});
+      })
+    }
+  })
+}
+
 
 module.exports.getUser = (req, res) => {
   try {
@@ -59,8 +86,14 @@ module.exports.login = async (req, res) => {
           payload = {
             id: result[0].id,
           };
-          accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
-          res.status(200).json({accessToken});
+          const accessToken = generateAccessToken(payload);
+          const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
+          connection.query(`INSERT INTO RefreshTokens (value) VALUES ("${refreshToken}")`, (err, result) => {
+            if (err) {
+              res.sendStatus(503);
+            }
+          });
+          res.status(200).json({accessToken, refreshToken});
         } else {
           res.sendStatus(401); // Invalid password
         }
@@ -76,29 +109,42 @@ module.exports.register = async (req, res) => {
     // Check if username has already been used
     connection.query(`SELECT id FROM Users WHERE username="${req.body.username}"`, async (err, result, field) => {
       if (err) {
-        // Could not connect to Database
-        res.sendStatus(503);
+        res.sendStatus(503); // Could not connect to Database
       }
       if (result.length) {
-        // User already exists
-        res.sendStatus(409);
+        res.sendStatus(409); // User already exists
       } else {
         // If non-existent, create new entry with req.body
         const hashPass = await bcrypt.hash(req.body.password, 10);
         var sql = `INSERT INTO Users (username, password, name, email) VALUES ("${req.body.username}", "${hashPass}", "${req.body.name}", "${req.body.email}")`
         connection.query(sql, (err, result) => {
           if (err) {
-            // Error inserting user
-            res.sendStatus(500);
+            res.sendStatus(500); // Error inserting user
           } else {
-            // Successfully added
-            res.sendStatus(201);
+            res.sendStatus(201); // Successfully added
           }
         })
       }
     })
   } catch {
     // Could not connect
+    res.sendStatus(500);
+  }
+}
+
+module.exports.logout = (req, res) => {
+  try {
+    const token = req.body.token;
+    if (!token) {
+      res.sendStatus(200);
+    }
+    connection.query(`DELETE FROM RefreshTokens WHERE value="${token}"`, (err, result) => {
+      if (err) {
+        res.sendStatus(401);
+      }
+      res.sendStatus(200);
+    })
+  } catch {
     res.sendStatus(500);
   }
 }
--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -7,5 +7,7 @@ router.use(express.json());
 router.get("/user", controller.authenticateToken, controller.getUser);
 router.post("/user", controller.login);
 router.post("/register", controller.register);
+router.post("/token", controller.refreshToken);
+router.delete("/logout", controller.logout);
 
 module.exports = router;
