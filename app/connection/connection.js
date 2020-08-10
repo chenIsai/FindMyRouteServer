9a32adf132f028764b72bfa6d30d22ef7e7864cd
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-10 18:15:20 -0400

    Updated connection

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -2,11 +2,7 @@ require("dotenv").config();
 
 const Pool = require("pg").Pool
 const connection = new Pool({
-  user: "api",
-  host: process.env.HOST_URL,
-  database: process.env.SQL_DATABASE,
-  password: process.env.SQL_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = connection;
