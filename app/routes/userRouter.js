097c9b1279882152ba5cc89fc11483c00a1a8a0d
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-16 19:22:07 -0400

    Updated route for login

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -4,8 +4,8 @@ const controller = require("../controllers/users");
 const router = express.Router();
 
 router.use(express.json());
-router.get("/user", controller.authenticateToken, controller.getUser);
-router.post("/user", controller.login);
+router.get("/login", controller.authenticateToken, controller.getUser);
+router.post("/loginr", controller.login);
 router.post("/register", controller.register);
 router.post("/token", controller.refreshToken);
 router.delete("/logout", controller.logout);
