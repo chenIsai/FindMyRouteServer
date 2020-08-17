0afb850bb0e525d7af1a53d5562cd66741835db5
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-17 16:17:13 -0400

    Removed unnecessary function

--- app/controllers/savedRoutes.js
+++ app/controllers/savedRoutes.js
@@ -2,25 +2,6 @@ const connection = require("../connection/connection");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports.authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (token == null) {
-    res.sendStatus(401);
-    return;
-  }
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      res.sendStatus(403);
-      return;
-    }
-    req.user = user;
-    next();
-  })
-}
-
 module.exports.saveRoute = (req, res) => {
   try {
     const token = req.body.token;
--- app/routes/savedRoutesRouter.js
+++ app/routes/savedRoutesRouter.js
@@ -4,7 +4,6 @@ const controller = require("../controllers/savedRoutes");
 const router = express.Router();
 
 router.use(express.json());
-router.use(controller.authenticateToken);
 router.get("/", controller.getRoutes);
 router.post("/", controller.saveRoute);
 router.delete("/", controller.deleteRoute);
