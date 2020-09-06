f22845b93cae4cefb05a9c4c9af22e20d08361af
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-09-06 17:08:54 -0400

    Update expiry for testing

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 const generateAccessToken = (user) => {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "600s"});
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "10s"});
 }
 
 const generateRefreshToken = (user) => {
